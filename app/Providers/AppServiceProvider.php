84e1b5a91c9ecd38679f9c5a189cb0731c441806
Halooween <saroja.grg33@gmail.com>
2022-11-08 11:37:12 +0545

    made changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('FORCE_HTTPS') === true) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
+
+   
 }
