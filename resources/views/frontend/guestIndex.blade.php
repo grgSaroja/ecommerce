b52237950b12f0c51be9c643be884088374eb38f
Halooween <saroja.grg33@gmail.com>
2022-11-08 13:40:34 +0545

    made changes

--- resources/views/frontend/guestIndex.blade.php
+++ resources/views/frontend/guestIndex.blade.php
@@ -1,15 +1,57 @@
-@extends('frontend.layout.master')
+{{-- @extends('frontend.layout.master') --}}
 		
 
-@section('content')
+{{-- @section('content') --}}
 
-	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ URL::asset('frontend/images/icons/favicon.png')}}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/bootstrap/css/bootstrap.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/fonts/linearicons-v1.0.0/icon-font.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/animate/animate.css')}}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/css-hamburgers/hamburgers.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/animsition/css/animsition.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/select2/select2.min.css')}}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/daterangepicker/daterangepicker.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/slick/slick.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/MagnificPopup/magnific-popup.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/vendor/perfect-scrollbar/perfect-scrollbar.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('frontend/css/main.css')}}">
+<!--===============================================================================================-->
+
+
+
+</head>
+
+<body class="animsition">
 	<!-- Product -->
 	<section class="bg0 p-t-23 p-b-140">
 		<div class="container">
 			<div class="p-b-10">
 				<h3 class="ltext-103 cl5">
-					Product Overview
+					Product Overview /page
 				</h3>
 			</div>
 
@@ -99,8 +141,9 @@
 			<i class="zmdi zmdi-chevron-up"></i>
 		</span>
 	</div>
-
-@endsection
+</body>
+</html>
+{{-- @endsection --}}
 	
 
 
