ec44303e58555cdb6bc4947508cc0a2b617a56a8
Halooween <saroja.grg33@gmail.com>
2022-11-08 12:15:41 +0545

    made changes

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'saroja.grg33.com'),
+        'name' => env('MAIL_FROM_NAME', 'saroja'),
     ],
 
     /*
