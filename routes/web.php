56695bb2d9fbe3e544bd2f6ef50775823e637a34
Halooween <saroja.grg33@gmail.com>
2022-11-08 11:53:48 +0545

    made changes

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,15 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\GuestController::class, 'index'])->name('index');
 Route::get('/gsearch', [App\Http\Controllers\GuestController::class, 'product_search'])->name('search');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/profile/edit/{id}', [App\Http\Controllers\HomeController::class, 'edit_profile'])->name('home.profile');
-Route::put('/profile/update/{id}', [App\Http\Controllers\HomeController::class, 'update_profile'])->name('profile.update');
-Route::get('/search', [App\Http\Controllers\HomeController::class, 'product_search'])->name('home.search');
+
 
 
 // forntend routes
 Route::group(['middleware' => ['auth', 'role:user'], 'prefix'=>'front'], function() { 
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile/edit/{id}', [App\Http\Controllers\HomeController::class, 'edit_profile'])->name('home.profile');
+Route::put('/profile/update/{id}', [App\Http\Controllers\HomeController::class, 'update_profile'])->name('profile.update');
+Route::get('/search', [App\Http\Controllers\HomeController::class, 'product_search'])->name('home.search');
 
 Route::get('/cart', [App\Http\Controllers\frontend\CartController::class, 'cart'])->name('cart');
 Route::post('update-cart', [App\Http\Controllers\frontend\CartController::class, 'update'])->name('cart.update');
